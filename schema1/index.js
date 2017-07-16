0ec233d3eacd6b6e2030dafb3dae71868f8db157
Jess Lin <perdendosi@gmail.com>
2017-07-15 17:31:46 -0700

    Add user + team types, schema1

--- schema1/index.js
+++ schema1/index.js
@@ -1,17 +1,85 @@
 // @flow
 
-var { buildSchema } = require('graphql');
+import {
+  GraphQLObjectType,
+  GraphQLList,
+  GraphQLSchema,
+  GraphQLString,
+} from 'graphql';
 
-var schema = buildSchema(`
-  type Query {
-    hello: String
-  }
-`);
 
-var root = { hello: () => 'Hello world!' };
+// Some dummy data
+const USER = {name: 'Alice'};
+const TEAM = {name: 'Team A'};
+USER.teams = [TEAM];
+TEAM.members = [USER];
+
+const FAKE_DB = {
+  User: USER,
+  Team: TEAM,
+};
+
+
+// Type definitions
+const userType = new GraphQLObjectType({
+  name: 'User',
+  fields: () => ({  // Must be a function to handle circular reference.
+    name: {
+      type: GraphQLString,
+      resolve: (parent, args, context) => {
+        return parent.name;
+      },
+    },
+    teams: {
+      type: new GraphQLList(teamType),
+      resolve: (parent, args, context) => {
+        return parent.teams;
+      },
+    }
+  }),
+});
+
+const teamType = new GraphQLObjectType({
+  name: 'Team',
+  fields: () => ({  // Must be a function to handle circular reference.
+    name: {
+      type: GraphQLString,
+      resolve: (parent, args, context) => {
+        return parent.name;
+      },
+    },
+    members: {
+      type: new GraphQLList(userType),
+      resolve: (parent, args, context) => {
+        return parent.members;
+      }
+    }
+  }),
+});
+
+
+const queryType = new GraphQLObjectType({
+  name: 'Query',
+  fields: {
+    user: {
+      type: userType,
+      args: {
+        id: {
+          type: GraphQLString,
+        },
+      },
+      resolve: (parent, args, context) => {
+        return FAKE_DB.User;
+      },
+    },
+  },
+});
+
+const schema = new GraphQLSchema({
+  query: queryType,
+});
 
 
 module.exports = {
   schema,
-  root,
 };
